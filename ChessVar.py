ae4411d43beaef0eb4db0eff907dd9e2958f8026
Josquin Larsen <jsqnlrsn@gmail.com>
2023-11-25 22:37:04 -0700

    added algebraic converter

--- ChessVar.py
+++ ChessVar.py
@@ -7,25 +7,89 @@ class ChessVar:
     """ a class to create an object of a ChessVar game"""
 
     def __init__(self):
-        self._game_state = 'UNFINISHED' # UNFINISHED, WHITE_WON, BLACK_WON
-        self._game_board = None  # list?
-        self._move_state = 'LEGAL' # ILLEGAL
+        self._game_state = 'UNFINISHED'  # 'UNFINISHED', 'WHITE_WON', 'BLACK_WON'
+        self._board_size = 8
+        self._game_board = []  # list?
         self._white_dict = {'K': 1, 'Q': 1, 'R': 2, 'B': 2, 'N': 2, 'P': 8}
         self._black_dict = {'k': 1, 'q': 1, 'r': 2, 'b': 2, 'n': 2, 'p': 8}
+        self._algebra_dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+        self._move_state = 'WHITE'  # 'BLACK'
+        self._white_counter = 1
+        self._black_counter = 1
 
     def get_game_state(self):
         """return game state"""
-        return self.get_game_state()
+        return self._game_state
 
     def set_game_state(self, game_state):
         """set/update game state"""
         self._game_state = game_state
 
-    def make_move(self, start_pos, end_pos):
+    def get_move_state(self):
+        """ return whose move it is"""
+        return self._move_state
+
+    def set_move_state(self, player:str):
+        """update move state 'WHITE' or 'BLACK' """
+        self._move_state = player
+
+    def get_white_counter(self):
+        """return number of white moves"""
+        return self._white_counter
+
+    def inc_white_counter(self):
+        """increment white counter"""
+        return self._white_counter + 1
+
+    def get_black_counter(self):
+        """ return black move counter"""
+        return self._black_counter
+
+    def inc_black_counter(self):
+        """ increment black counter"""
+        return self._black_counter + 1
+
+    def convert_algebraic(self, position: str):
+        """
+        converts algebraic notation to 2d array coordinates,
+        returns tuple for unpacking
+        """
+        # decorator?
+        row = 0
+        column = 0
+        for character in position:
+            letter = character.lower()
+            if position[0] not in self._algebra_dict or int(position[1]) > 8:
+                return False
+            else:
+                if letter in self._algebra_dict:
+                    row += (self._algebra_dict[letter])
+                    column += (int(position[1])) - 1
+
+        return row, column
+
+    def make_move(self, start_pos: str, end_pos: str):
         """return False if move is illegal, or game is already won"""
         if self._game_state != 'UNFINISHED':
             return False
-        pass
+
+
+    def make_board(self):
+        """ create board for display"""
+        board_size = self._board_size
+        for row in range(board_size):
+            board_row = []
+            for column in range(board_size):
+                board_row.append("_")
+            self._game_board.append(board_row)
+
+    def print_board(self):
+        """print nice board"""
+        for row in self._game_board:
+            printed_row = "  "
+            for column in row:
+                printed_row += column
+            print(printed_row)
 
     # TODO
 
@@ -129,3 +193,12 @@ class ChessVar:
         # TODO
         pass
 
+def main():
+    cv = ChessVar()
+    cv.make_board()
+    cv.print_board()
+    print(cv.convert_algebraic('b8'))
+
+
+if __name__ == '__main__':
+    main()
